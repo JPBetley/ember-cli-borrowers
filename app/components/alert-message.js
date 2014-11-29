6f3a4d98db892beaa9053b60ce702777c580630d
John Phillip Betley <jpbetley@gmail.com>
2014-11-29 10:27:05 -0500

    optimize selector code for alert message

--- app/components/alert-message.js
+++ app/components/alert-message.js
@@ -15,17 +15,17 @@ export default Ember.Component.extend(TypeClass, {
       Ember.run.later(_this, 'dismiss', _this.dismissAfter);
     }
 
-    Ember.$("#" + this.elementId).bind('closed.bs.alert', function() {
+    _this.$().bind('closed.bs.alert', function() {
       _this.sendAction('closed', _this.get('closedParam'));
       return _this.destroy();
     });
 
-    return Ember.$("#" + this.elementId).bind('close.bs.alert', function() {
+    return _this.$().bind('close.bs.alert', function() {
       return _this.sendAction('close', _this.get('closedParam'));
     });
   },
 
   dismiss: function() {
-    return Ember.$("#" + this.elementId).alert('close');
+    return this.$().alert('close');
   }
 });
