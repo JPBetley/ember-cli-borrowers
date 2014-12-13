edd3a90df14fdde7179ffe5a6e271e39b28ffea9
John Betley <jpbetley@gmail.com>
2014-12-13 17:12:50 -0500

    use component over view for nav

---
+++ app/components/nav-item.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	tagName: 'li',
+  classNameBindings: ['active'],
+
+  active: Ember.computed('childViews.firstObject.active', function() {
+    return this.get('childViews.firstObject.active');
+  })
+});
